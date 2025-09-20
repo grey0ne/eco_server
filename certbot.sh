df3b985cff71d9c55f58d6698b054d29427be491
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-09-20 23:24:18 +0500

    added cert init

--- certbot.sh
+++ certbot.sh
@@ -5,5 +5,5 @@ CHALLENGE_VOLUME=./nginx/ssl/challenge
 NGINX_CONTAINER=$(docker ps -q -f name=nginx)
 
 
-docker run --rm --name temp_certbot -v $CERTS_VOLUME:/etc/letsencrypt -v $CHALLENGE_VOLUME:/tmp/letsencrypt certbot/certbot:v1.14.0 renew --webroot --agree-tos --no-random-sleep-on-renew -w /tmp/letsencrypt
+docker run --rm --name temp_certbot -v $CERTS_VOLUME:/etc/letsencrypt -v $CHALLENGE_VOLUME:/tmp/letsencrypt certbot/certbot:v1.14.0 certonly --webroot --agree-tos --no-random-sleep-on-renew -w /tmp/letsencrypt -d eco.grey.one
 docker exec $NGINX_CONTAINER nginx -s reload
